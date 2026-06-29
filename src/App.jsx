f17ae79dccc9965ee0280bfe05db1d38b976ef95
Tu Nombre <tu.email@ejemplo.com>
2026-06-29 10:52:19 -0500

    chore: eliminar documentación

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Catalogo from './views/Catalogo';
 import Carrito from './views/Carrito';
 import Admin from './views/Admin';
 import Contacto from './views/Contacto';
-import Documentacion from './views/Documentacion';
 
 export default function App(){
   return <CartProvider>
@@ -19,7 +18,6 @@ export default function App(){
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/contacto" element={<Contacto />} />
-        <Route path="/documentacion" element={<Documentacion />} />
       </Routes>
       <a href="https://wa.me/51999999999" target="_blank" className="fixed right-5 bottom-5 z-50 bg-emerald-500 text-white w-16 h-16 rounded-full grid place-items-center text-3xl shadow-2xl">☘</a>
       <Footer />
